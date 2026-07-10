96316ca9b198bf89d193e602f526b6452061ae1a
Shri Nivas <mrshri404@gmail.com>
2026-07-10 22:35:15 +0530

    Refactor hero component and styles; update navbar for mobile responsiveness; enhance project and skills sections with new layouts and themes; improve light theme styles across the application; remove outdated footer content; optimize code structure and readability.

--- src/app/models/Data/skills.ts
+++ src/app/models/Data/skills.ts
@@ -1,64 +1,116 @@
-import { Skill } from "../skill.model";
+import { Skill } from '../skill.model';
 
 export const SKILLS: Skill[] = [
+  {
+    name: 'C# Programming',
+    value: 95,
+    category: 'Language',
+  },
+  {
+    name: 'Typescript',
+    value: 95,
+    category: 'Language',
+  },
+  {
+    name: 'JavaScript',
+    value: 95,
+    category: 'Language',
+  },
 
-    {
-        name: "C# Programming",
-        value: 95,
-        category: "Backend"
-    },
+  {
+    name: 'ASP.NET Core',
+    value: 95,
+    category: 'Backend',
+  },
 
-    {
-        name: "ASP.NET Core",
-        value: 95,
-        category: "Backend"
-    },
+  {
+    name: 'Angular',
+    value: 90,
+    category: 'Frontend',
+  },
 
-    {
-        name: "Angular",
-        value: 90,
-        category: "Frontend"
-    },
+  {
+    name: 'SQL Server',
+    value: 92,
+    category: 'Database',
+  },
+  {
+    name: 'Linq',
+    value: 92,
+    category: 'Database',
+  },
+  {
+    name: 'Stored Procedures',
+    value: 92,
+    category: 'Database',
+  },
 
+  {
+    name: 'Entity Framework Core',
+    value: 90,
+    category: 'Backend',
+  },
 
-    {
-        name: "SQL Server",
-        value: 92,
-        category: "Database"
-    },
+  {
+    name: 'Azure',
+    value: 80,
+    category: 'Cloud',
+  },
+  {
+    name: 'Azure DevOps',
+    value: 80,
+    category: 'Cloud',
+  },
+  {
+    name: 'azure App Service',
+    value: 80,
+    category: 'Cloud',
+  },
 
-    {
-        name: "Entity Framework Core",
-        value: 90,
-        category: "Backend"
-    },
+  {
+    name: 'REST API',
+    value: 95,
+    category: 'Backend',
+  },
 
-    {
-        name: "Azure",
-        value: 80,
-        category: "Cloud"
-    },
-
-    {
-        name: "REST API",
-        value: 95,
-        category: "Backend"
-    },
-
-    {
-        name: "Git",
-        value: 90,
-        category: "Tools"
-    },
-      {
-        name: "Microservices",
-        value: 80,
-        category: "Backend"
-    },
-      {
-        name: "ASP.NET Core MVC",
-        value: 90,
-        category: "Backend"
-    }
-
+  {
+    name: 'Git',
+    value: 90,
+    category: 'Tools',
+  },
+  {
+    name: 'visual studio',
+    value: 90,
+    category: 'Tools',
+  },
+  {
+    name: 'vs code',
+    value: 90,
+    category: 'Tools',
+  },
+  {
+    name: 'Postman',
+    value: 90,
+    category: 'Tools',
+  },
+  {
+    name: 'Swagger',
+    value: 90,
+    category: 'Tools',
+  },
+  {
+    name: 'ServiceNow',
+    value: 90,
+    category: 'Tools',
+  },
+  {
+    name: 'Microservices',
+    value: 80,
+    category: 'Backend',
+  },
+  {
+    name: 'ASP.NET Core MVC',
+    value: 90,
+    category: 'Backend',
+  },
 ];
